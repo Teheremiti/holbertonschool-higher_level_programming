fac779910809dd686fa8a4b3a9387348cd67cd1d
Teheremiti <t.tuiaiho@gmail.com>
2023-05-31 10:19:28 -0700

    Fix shebang

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def uppercase(str):
     for letter in str:
         _ascii = ord(letter)
         if _ascii >= 97 and _ascii <= 122:
             _ascii -= 32
         print("{}".format(chr(_ascii)), end="")
-    print("\n", end="")
+    print("")
