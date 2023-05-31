bb276cfc39a579f7b78b1a896861e695143536fe
Teheremiti <t.tuiaiho@gmail.com>
2023-05-31 09:22:53 -0700

    Without variables

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-sep = ""
 
 for tens in range(9):
     for units in range(tens + 1, 10):
         if (tens != units):
-            print(f"{sep}{tens}{units}", end="")
-
-        sep = ", "
+            print(f"{tens}{units}", end="")
+        if (tens != 8):
+            print(", ", end="")
 
 print()
