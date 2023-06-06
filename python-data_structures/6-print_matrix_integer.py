dd5cee2221e89dc3979c72dd744453fe33540670
Teheremiti <t.tuiaiho@gmail.com>
2023-06-06 04:14:17 -0700

    Print a matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for sub in matrix:
+        sep = ""
+
+        for ele in sub:
+            print("{}{:d}".format(sep, ele), end="")
+            sep = " "
+
+        print("$")
