33a5f5ccbb2614c87e9a6608431d61771053b9ce
Teheremiti <t.tuiaiho@gmail.com>
2023-06-06 05:43:45 -0700

    Use reversed() function

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if my_list == []:
         return
 
-    for i in range(1, len(my_list) + 1):
-        print("{:d}".format(my_list[-i]))
+    for ele in reversed(my_list):
+        print("{:d}".format(ele))
