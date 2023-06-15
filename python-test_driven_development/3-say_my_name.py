b0744357a84128ad2e7f12c69b5fa07de8833d47
Teheremiti <t.tuiaiho@gmail.com>
2023-06-15 04:42:09 -0700

    Print out a name in a test-driven environment

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module with a function that prints out its arguments in a sentence"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print out the name and last name passed as arguments in a sentence.
+
+    Args:
+        first_name (string): First name to print.
+        last_name (string, optional): Last name to print. Defaults to "".
+
+    Raises:
+        TypeError: if either of the two arguments is not a string.
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
