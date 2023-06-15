86b73bc44ace7f0e73091b30954d2ad2a0e7d885
Teheremiti <t.tuiaiho@gmail.com>
2023-06-15 05:02:06 -0700

    Print a square of '#'s in a test-driven environment

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """Module with a function that prints out a square of '#'s"""
 
+
 def print_square(size):
     """Print out a square with the char '#' of the size determined by the
-    'size' argument."""
+    'size' argument.
+
+    Args:
+        size (int): Positive integer representing the length of the square.
+
+    Raises:
+        TypeError: if size is not an integer.
+        ValueError: if size is less than zero.
+    """
 
     if type(size) is not int:
         raise TypeError("size must be an integer")
