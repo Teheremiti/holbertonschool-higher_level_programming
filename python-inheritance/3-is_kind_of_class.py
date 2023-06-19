eb12b596741df74b050984f35cb1c2c04a325355
Teheremiti <t.tuiaiho@gmail.com>
2023-06-19 13:39:05 -0700

    pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@ a class or one of its derivated classes"""
 
 
 def is_kind_of_class(obj, a_class):
-    """Check if an object is an instance of a specified class or 
+    """Check if an object is an instance of a specified class or
     one of its derivatives"""
 
     return isinstance(obj, a_class)
