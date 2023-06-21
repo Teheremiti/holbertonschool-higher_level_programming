e5fa45a42e68508b99f14ca21623e8ce1b2cce9f
Teheremiti <t.tuiaiho@gmail.com>
2023-06-21 02:26:33 -0700

    Add integer_validator() method

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module where the BaseGeometry class is defined"""
+
+
+class BaseGeometry:
+    """Defines the BaseGeometry class"""
+
+    def area(self):
+        """Computes the area of a shape"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Checks if 'value' has the excepted type and within the excpected
+        range.
+
+        Args:
+            name (string): The object attribute's name
+            value (unknown): The object attribute's value
+
+        Raises:
+            TypeError: if 'value' is not an integer
+            ValueError: if 'value' is less or equal to zero
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
