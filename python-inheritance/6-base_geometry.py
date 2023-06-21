71aa6011fa1f4206a863ba6d00fbd5b36c0deebe
Teheremiti <t.tuiaiho@gmail.com>
2023-06-21 01:56:01 -0700

    Add area() function but does nothing yet

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module where the BaseGeometry class is defined"""
+
+
+class BaseGeometry:
+    """Defines the BaseGeometry class"""
+
+    def area(self):
+        """Computes the area of a shape"""
+        raise Exception("area() is not implemented")
