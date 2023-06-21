d9fd90b17585a6f91961bc9eec19a42090ec8dc4
Teheremiti <t.tuiaiho@gmail.com>
2023-06-21 02:29:34 -0700

    Create Rectangle subclass

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module where the Rectangle class is defined"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines the Rectangle class which inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initializes the object's attributes during instanciation
+
+        Args:
+            width (int): Defines the width of the object
+            height (int): Defines the height of the object
+        """
+
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
