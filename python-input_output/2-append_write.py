65d590da7ffe9d683c45cc262f2ca5d4effc4845
Teheremiti <t.tuiaiho@gmail.com>
2023-06-21 07:42:11 -0700

    Complete comments

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,15 @@
 
 
 def append_write(filename="", text=""):
-    """Appends the string 'text' to the end of the file"""
+    """Appends the string 'text' to the end of the file
+
+    Args:
+        filename (str): File to append the content of 'text' to
+        text (str): String to append at the end of the file
+
+    Returns:
+        The number of characters written
+    """
 
     with open(filename, 'a') as f:
         return f.write(text)
