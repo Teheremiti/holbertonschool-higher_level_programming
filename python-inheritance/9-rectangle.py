a7f410dd17ba65f07e4bf6bf61521508f7e550a0
Teheremiti <t.tuiaiho@gmail.com>
2023-06-22 03:56:28 -0700

    Fix indentiation

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -21,10 +21,12 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-        def area(self):
-            """Computes and returns the area of the rectangle"""
-            return self.__width * self.__height
+    def area(self):
+        """Computes and returns the area of the rectangle"""
 
-        def __str__(self):
-            """Returns the string representation of the rectangle"""
-            return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the string representation of the rectangle"""
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
