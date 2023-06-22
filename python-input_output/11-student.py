5aed6213466fb985e07e2be5d37622a54cefb39c
Teheremiti <t.tuiaiho@gmail.com>
2023-06-22 02:14:18 -0700

    Add reload_from_json() method that replaces all attibutes of an instance

---
+++ python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module that defines the Student class"""
+
+
+class Student:
+    """Defines the Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor method for the Student class.
+
+        Args:
+            first_name (str): The student's first name
+            last_name (str): The student's last name
+            age (int): The student's age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation for JSON serialization of
+        an object"""
+
+        if type(attrs) is list and (attr is str for attr in attrs):
+            my_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    my_dict[attr] = self.__dict__[attr]
+
+            return my_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+
+        for attr, value in json.items():
+            if attr in self.__dict__:
+                self.__dict__[attr] = value
