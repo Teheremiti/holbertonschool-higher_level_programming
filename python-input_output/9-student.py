8fe3e4fc3fa1a64df75690587623f7d454331805
Teheremiti <t.tuiaiho@gmail.com>
2023-06-22 01:41:48 -0700

    DocStrings

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,9 +6,20 @@ class Student:
     """Defines the Student class"""
 
     def __init__(self, first_name, last_name, age):
+        """Constructor method for the Student class.
+
+        Args:
+            first_name (str): The student's first name
+            last_name (str): The student's last name
+            age (int): The student's age
+        """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Returns the dictionary representation for JSON serialization of
+        an object"""
+
         return self.__dict__
