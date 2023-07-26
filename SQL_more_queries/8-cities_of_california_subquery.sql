087541aea326e74abb382c256a34e2840df2482b
Teheremiti <t.tuiaiho@gmail.com>
2023-07-26 16:07:50 -0700

    Select only the id and name columns

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- List all the cities of California found in the database hbtn_0d_usa
-SELECT * FROM hbtn_0d_usa.cities
+SELECT id, name FROM cities
 WHERE state_id = (
-	SELECT id FROM hbtn_0d_usa.states
+	SELECT id FROM states
 	WHERE name = 'California')
-ORDER BY id;
+ORDER BY cities.id;
