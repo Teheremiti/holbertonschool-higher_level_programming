f57b7e016a0c4c7600b9ab5552f550030c77170e
Teheremiti <t.tuiaiho@gmail.com>
2023-08-02 13:58:51 -0700

    Create a State class to objectify columns from the states class

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module containing the definition of the State class"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+class State(Base):
+    """Defines the States class which is linked to the states table"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
