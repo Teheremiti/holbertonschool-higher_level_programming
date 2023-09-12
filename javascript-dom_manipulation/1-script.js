4258bd7da763aab4f059c0c6c4b2d3f715d0b3bd
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-09-12 16:24:05 +0200

    Update a property on click from the user

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,7 @@
+const red_header = document.querySelector("#red_header");
+
+red_header.addEventListener("click", updateColor);
+
+function updateColor() {
+    red_header.style.color = "#FF0000"
+}
